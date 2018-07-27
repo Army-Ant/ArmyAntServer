856317413eb49f0118b3cb8ab4b39a62d369080e
Jason-Zhao-Jie <261343578@qq.com>
2018-07-27 17:06:39 +0800

    Update ArmyAntLib by fixed some bugs, see https://github.com/Army-Ant/ArmyAntLib.git for its commit version cbbcb1bd9cbb9c27725de88dcf21005e0336d1dc Add Windows-Application resources frame

---
+++ inc/WindowsResourcesManager.h
@@ -0,0 +1,49 @@
+#ifndef WINDOWS_RESOURCES_MANAGER_H_20180724
+#define WINDOWS_RESOURCES_MANAGER_H_20180724
+
+#include <stdafx.h>
+#include <AADefine.h>
+#include <functional>
+
+class WindowsResourcesManager{
+public:
+	static WindowsResourcesManager*getInstance();
+	static void releaseInstance();
+
+public:
+	enum class Window{
+		Default
+	};
+	enum class Control{
+		Window_Default
+	};
+	enum class EventType{
+		Create
+	};
+	typedef std::function<bool(Control, EventType)> EventCallback;
+
+public:
+	bool attach(HINSTANCE handle);
+	void dettach();
+
+	bool registerEvent(Window win, Control control, EventType event, EventCallback cb);
+	bool unregisterEvent(Window win, Control control, EventType event);
+
+	bool openWindow(Window win);
+	bool closeWindow(Window win);
+
+private:
+	HINSTANCE sysHandle;
+
+private:
+	static WindowsResourcesManager* instance;
+
+private:
+	WindowsResourcesManager();
+	~WindowsResourcesManager();
+
+	AA_FORBID_COPY_CTOR(WindowsResourcesManager);
+	AA_FORBID_ASSGN_OPR(WindowsResourcesManager);
+};
+
+#endif // WINDOWS_RESOURCES_MANAGER_H_20180724
