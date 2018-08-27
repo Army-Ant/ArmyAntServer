a2e20eeddcf9460936cc0f72518b2e4869bf9bf4
Jason-Zhao-Jie <261343578@qq.com>
2018-08-27 20:59:07 +0800

    Add message defined proto file between ServerMain and DBProxy

--- scripts/compile_protobuf.bat
+++ scripts/compile_protobuf.bat
@@ -12,5 +12,6 @@ set PROTOBINARY_PATH=..\res\ProtobufSource\pb
 
 %PROTOC_PATH%\protoc.exe -I=%PROTOFILE_PATH% --cpp_out=%PROTOCPP_PATH% --csharp_out=%PROTOCSHARP_PATH% --js_out=%PROTOJAVASCRIPT_PATH% --python_out=%PROTOPYTHON_PATH% --ruby_out=%PROTORUBY_PATH% %PROTOFILE_PATH%\ArmyAntMessage\System\SocketHead.proto
 %PROTOC_PATH%\protoc.exe -I=%PROTOFILE_PATH% --cpp_out=%PROTOCPP_PATH% --csharp_out=%PROTOCSHARP_PATH% --js_out=%PROTOJAVASCRIPT_PATH% --python_out=%PROTOPYTHON_PATH% --ruby_out=%PROTORUBY_PATH% %PROTOFILE_PATH%\ArmyAntMessage\System\SessionStart.proto
+%PROTOC_PATH%\protoc.exe -I=%PROTOFILE_PATH% --cpp_out=%PROTOCPP_PATH% --csharp_out=%PROTOCSHARP_PATH% --js_out=%PROTOJAVASCRIPT_PATH% --python_out=%PROTOPYTHON_PATH% --ruby_out=%PROTORUBY_PATH% %PROTOFILE_PATH%\ArmyAntMessage\DBProxy\SqlRequest.proto
 
 pause
