dad219a2a25d48899a1dd13b4b936c591e77ba89
jason <jason@debian.jason.com>
2018-09-05 18:27:38 +0800

    Fixed errors of sql script in Linux MariaDB

--- res/sqlScripts/initializeNewEmptyDB.sql
+++ res/sqlScripts/initializeNewEmptyDB.sql
@@ -4,11 +4,10 @@ use ArmyAntDB;
 create table app_list (id int, appid bigint, name varchar(512));
 
 CREATE USER 'aa_server_dbproxy'@'%' IDENTIFIED BY 'L05rg05rgP8K';
-GRANT ALL ON ArmyAntDB.* TO 'aa_server_dbproxy'@'%';
+GRANT ALL PRIVILEGES ON ArmyAntDB.* TO 'aa_server_dbproxy'@'%';
 CREATE USER 'aa_db_guest'@'%' IDENTIFIED BY '0624zjcy0225';
 GRANT select ON ArmyAntDB.* TO 'aa_db_guest'@'%';
 
 use mysql;
-update user set host = '%'  where user ='root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '05rgP8KL05rg' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '05rgP8KL05rg';
--- scripts/compile_protobuf.sh
+++ scripts/compile_protobuf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo off
+echo Start to compile protobuf ...
 
 PROTOC_PATH=../external/Protobuf/bin
 PROTOFILE_PATH=../proto
@@ -12,9 +12,28 @@ PROTOPYTHON_PATH=../res/ProtobufSource/python
 PROTORUBY_PATH=../res/ProtobufSource/ruby
 PROTOBINARY_PATH=../res/ProtobufSource/pb
 
+if [ ! -d $PROTOCPP_PATH ]; then
+    mkdir $PROTOCPP_PATH
+fi
+if [ ! -d $PROTOCSHARP_PATH ]; then
+    mkdir $PROTOCSHARP_PATH
+fi
+if [ ! -d $PROTOJAVASCRIPT_PATH ]; then
+    mkdir $PROTOJAVASCRIPT_PATH
+fi
+if [ ! -d $PROTOPYTHON_PATH ]; then
+    mkdir $PROTOPYTHON_PATH
+fi
+if [ ! -d $PROTORUBY_PATH ]; then
+    mkdir $PROTORUBY_PATH
+fi
+if [ ! -d $PROTOBINARY_PATH ]; then
+    mkdir $PROTOBINARY_PATH
+fi
 
 ${PROTOC_PATH}/protoc -I=$PROTOFILE_PATH --cpp_out=$PROTOCPP_PATH --csharp_out=$PROTOCSHARP_PATH --js_out=$PROTOJAVASCRIPT_PATH --python_out=$PROTOPYTHON_PATH --ruby_out=$PROTORUBY_PATH $PROTOFILE_PATH/ArmyAntMessage/System/SocketHead.proto
 ${PROTOC_PATH}/protoc -I=$PROTOFILE_PATH --cpp_out=$PROTOCPP_PATH --csharp_out=$PROTOCSHARP_PATH --js_out=$PROTOJAVASCRIPT_PATH --python_out=$PROTOPYTHON_PATH --ruby_out=$PROTORUBY_PATH $PROTOFILE_PATH/ArmyAntMessage/System/SessionStart.proto
 ${PROTOC_PATH}/protoc -I=$PROTOFILE_PATH --cpp_out=$PROTOCPP_PATH --csharp_out=$PROTOCSHARP_PATH --js_out=$PROTOJAVASCRIPT_PATH --python_out=$PROTOPYTHON_PATH --ruby_out=$PROTORUBY_PATH $PROTOFILE_PATH/ArmyAntMessage/DBProxy/SqlRequest.proto
 
-pause
+read -rsp $'Finished !\nPress any key to exit.\n' -n 1 key
+# echo $key
