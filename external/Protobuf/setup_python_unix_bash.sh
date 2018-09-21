1d838071098d77629dad05815e6bbf2b1b634cc6
Jason-Zhao-Jie <261343578@qq.com>
2018-09-21 10:30:12 +0800

    remove protobuf

--- external/Protobuf/setup_python_unix_bash.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-PATH=$PATH;$CD\bin
-cd .\python-not-compiled
-python setup.py build
-python setup.py install
--- external/Protobuf/setup_python_windows.bat
+++
@@ -1,4 +0,0 @@
-set PATH=%PATH%;%CD%\bin
-cd .\python-not-compiled
-python setup.py build
-python setup.py install
