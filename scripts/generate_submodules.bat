6b5b5e96715247a93170bd13e4e31a13a498f75e
Jason-Zhao-Jie <261343578@qq.com>
2018-09-21 16:34:10 +0800

    update sub-modules auto-generating script

---
+++ scripts/generate_submodules.bat
@@ -0,0 +1,9 @@
+cd ..
+git submodule update --init --progress
+cd external
+cd ArmyAntLib
+cd external
+start boost_generate_and_build.bat
+cd ../..
+cd Protobuf
+git submodule update --init --progress
