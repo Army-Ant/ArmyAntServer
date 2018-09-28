89cb020b13ebca03e4ecc66478e2183c53fa9c00
Jason-Zhao-Jie <261343578@qq.com>
2018-09-28 10:26:45 +0800

    Update submodules and its generation scripts

---
+++ external/build_protobuf.bat
@@ -0,0 +1,13 @@
+cd Protobuf/cmake
+mkdir build_win
+cd build_win
+mkdir debug
+mkdir release
+cd debug
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../../.. ../..
+nmake
+nmake install
+cd ../release
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../.. ../..
+nmake
+nmake install
--- scripts/generate_submodules.bat
+++ scripts/generate_submodules.bat
@@ -6,7 +6,7 @@ cd external
 :GENERATE_BOOST
 cd ArmyAntLib
 cd external
-start boost_generate_and_build.bat
+call boost_generate_and_build.bat
 
 :GENERATE_PROTOBUF
 cd ../..
@@ -14,18 +14,7 @@ cd Protobuf
 git submodule update --init --progress --recursive
 
 :BUILD_PROTOBUF
-cd cmake
-mkdir build_win
-cd build_win
-mkdir debug
-mkdir release
-cd debug
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=../../.. ../..
-nmake
-nmake install
-cd ../release
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../.. ../..
-nmake
-nmake install
+cd ..
+call build_protobuf.bat
 
 :END
