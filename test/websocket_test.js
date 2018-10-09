66ea46a07a47472ffd3ab7807052d3ade932d895
Jason-Zhao-Jie <261343578@qq.com>
2018-10-09 16:53:23 +0800

    Fixed most bugs in javascript tester

--- test/websocket_test.js
+++ test/websocket_test.js
@@ -1,54 +1,80 @@
-'use strict'
+﻿'use strict'
+
+import libArmyAnt from "./ArmyAnt.js/libArmyAnt.js"
 
 var websocket_tester = {
 	_ws:null,
 	_googAddlibList:[
 	],
 	_scriptList:[
-		"libprotobuf-js/map.js",
-		"libprotobuf-js/message.js",
-		"libprotobuf-js/binary/constants.js",
-		"libprotobuf-js/binary/utils.js",
-		"libprotobuf-js/binary/arith.js",
-		"libprotobuf-js/binary/encoder.js",
-		"libprotobuf-js/binary/decoder.js",
-		"libprotobuf-js/binary/reader.js",
-		"libprotobuf-js/binary/writer.js",
-		"proto-js/c2sm_echologinrequest.js",
-		"proto-js/sm2c_echologinresponse.js",
-		"proto-js/c2sm_echologoutrequest.js",
-		"proto-js/sm2c_echologoutresponse.js",
-		"proto-js/c2sm_echosendrequest.js",
-		"proto-js/sm2c_echosendresponse.js",
-		"proto-js/c2sm_echobroadcastrequest.js",
-		"proto-js/sm2c_echoerror.js",
-		"proto-js/sm2c_echoreceivenotice.js",
-		"proto-js/sm2c_echobroadcastresponse.js",
+		{script: "libprotobuf-js/map.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/message.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/binary/constants.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/binary/utils.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/binary/decoder.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/binary/encoder.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/binary/arith.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/binary/reader.js", isAsync: false, isDefer: false},
+		{script: "libprotobuf-js/binary/writer.js", isAsync: false, isDefer: false},
+		{script: "proto-js/c2sm_echologinrequest.js", isAsync: false, isDefer: false},
+		{script: "proto-js/sm2c_echologinresponse.js", isAsync: false, isDefer: false},
+		{script: "proto-js/c2sm_echologoutrequest.js", isAsync: false, isDefer: false},
+		{script: "proto-js/sm2c_echologoutresponse.js", isAsync: false, isDefer: false},
+		{script: "proto-js/c2sm_echosendrequest.js", isAsync: false, isDefer: false},
+		{script: "proto-js/sm2c_echosendresponse.js", isAsync: false, isDefer: false},
+		{script: "proto-js/c2sm_echobroadcastrequest.js", isAsync: false, isDefer: false},
+		{script: "proto-js/sm2c_echoerror.js", isAsync: false, isDefer: false},
+		{script: "proto-js/sm2c_echoreceivenotice.js", isAsync: false, isDefer: false},
+		{script: "proto-js/sm2c_echobroadcastresponse.js", isAsync: false, isDefer: false},
 	],
 	
 	echoServerRequiring: function(){
-		for(let i=0; i<websocket_tester._scriptList.length; ++i){
-			libArmyAnt.importScript(websocket_tester._scriptList[i])
+		let loadScript = (i) => {
+			if(i >= websocket_tester._scriptList.length)
+				return;
+			let scriptName = websocket_tester._scriptList[i].script;
+			let isAsync = false
+			if(websocket_tester._scriptList[i].isAsync)
+				isAsync = websocket_tester._scriptList[i].isAsync
+			let isDefer = false
+			if(websocket_tester._scriptList[i].isDefer)
+				isDefer = websocket_tester._scriptList[i].isDefer
+			libArmyAnt.importScript(scriptName, isAsync, isDefer, ()=>{loadScript(i+1);})
 			//goog.require(websocket_tester._scriptList[i])
+		}
+		loadScript(0);
+	},
+	
+	onload: function(){
+		websocket_tester._ws = new WebSocket("ws://127.0.0.1:14774/")
+		websocket_tester._ws.onopen = function(evt) { 
+		  console.log("Websocket opened");
+		  document.getElementById('tester').innerHTML += '<br/>' + "WebSocket 服务器已连接"
+		  //websocket_tester._ws.send("Hello WebSockets!");
+		};
+
+		websocket_tester._ws.onmessage = function(evt) {
+		  console.log( "Received Message: " + evt)
+		  //websocket_tester._ws.close()
+		}
+
+		websocket_tester._ws.onclose = function(evt) {
+		  console.log("Websocket closed.")
+		  document.getElementById('tester').innerHTML += '<br/>' + "WebSocket 服务器已断开"
 		}
 	},
 	
-	socketConnect: function(addr, port){
-		websocket_tester._ws = new WebSocket("ws://"+addr+":"+port+"/")
-		ws.onopen = function(evt) { 
-		  console.log("Connection open ...");
-		  ws.send("Hello WebSockets!")
-		};
-
-		ws.onmessage = function(evt) {
-		  console.log( "Received Message: " + evt.data)
-		  ws.close()
-		}
-
-		ws.onclose = function(evt) {
-		  console.log("Connection closed.")
-		}
-	}
+	connect: function(){
+	},
+	
+	login: function(){
+	},
+	
+	send: function(){
+	},
+	
+	broadcast: function(){
+	},
 }
 
 websocket_tester.echoServerRequiring();
