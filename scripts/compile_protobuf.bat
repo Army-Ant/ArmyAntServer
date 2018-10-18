78d485115164da91e9ee89518ef2f91f6535f823
Jason-Zhao-Jie <261343578@qq.com>
2018-10-18 17:02:58 +0800

    add client tools

--- scripts/compile_protobuf.bat
+++ scripts/compile_protobuf.bat
@@ -14,12 +14,15 @@ set PROTOPYTHON_PATH=%PROTOSRC_BASE%\python
 set PROTOCOMMONJS_PATH=..\bin
 
 :set_source_file_list
+::base
 set PROTOFILES=%PROTOFILE_PATH%\ArmyAntMessage\Common\base.proto
 set PROTOFILES=%PROTOFILES% %PROTOFILE_PATH%\ArmyAntMessage\System\SocketHead.proto
 set PROTOFILES=%PROTOFILES% %PROTOFILE_PATH%\ArmyAntMessage\System\SessionStart.proto
-set PROTOFILES=%PROTOFILES% %PROTOFILE_PATH%\ArmyAntMessage\DBProxy\SqlRequest.proto
+::sub application
 set PROTOFILES=%PROTOFILES% %PROTOFILE_PATH%\ArmyAntMessage\SubApps\SimpleEcho.proto
 set PROTOFILES=%PROTOFILES% %PROTOFILE_PATH%\ArmyAntMessage\SubApps\Huolong.proto
+::database proxy
+set PROTODBPROXYFILES=%PROTOFILE_PATH%\ArmyAntMessage\DBProxy\SqlRequest.proto
 
 :create_noexist_path
 if not exist %PROTOCPP_PATH% (mkdir %PROTOCPP_PATH%)
@@ -30,7 +33,7 @@ if not exist %PROTOPYTHON_PATH% (mkdir %PROTOPYTHON_PATH%)
 ::if not exist %PROTOBINARY_PATH% (mkdir %PROTOBINARY_PATH%)
 
 :execute_protoc
-%PROTOC_PATH%\protoc.exe -I=%PROTOFILE_PATH% --cpp_out=%PROTOCPP_PATH% --csharp_out=%PROTOCSHARP_PATH% --js_out=library=aaserver_proto,binary:%PROTOJAVASCRIPT_PATH% --python_out=%PROTOPYTHON_PATH% %PROTOFILES%
+%PROTOC_PATH%\protoc.exe -I=%PROTOFILE_PATH% --cpp_out=%PROTOCPP_PATH% --csharp_out=%PROTOCSHARP_PATH% --js_out=library=aaserver_proto,binary:%PROTOJAVASCRIPT_PATH% --python_out=%PROTOPYTHON_PATH% %PROTOFILES% %PROTODBPROXYFILES%
 %PROTOC_PATH%\protoc.exe -I=%PROTOFILE_PATH% --js_out=import_style=commonjs,binary:%PROTOCOMMONJS_PATH% %PROTOFILES%
 
 :End
